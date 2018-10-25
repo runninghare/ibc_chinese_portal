388d8f1e6e5d863835354f3809dc998652c14a10
rossz <rzhu@pegasus.net.au>
2018-10-25 16:43:25 +1100

    Remove useless imports

--- src/pages/app-link/app-link.ts
+++ src/pages/app-link/app-link.ts
@@ -1,7 +1,6 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CommonProvider } from '../../providers/common/common';
-import * as $ from 'jquery';
 
 /**
  * Generated class for the AppLinkPage page.
