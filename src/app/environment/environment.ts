74057dba8e8c642bc98f5bf42ead92ab3b425919
rossz <rzhu@pegasus.net.au>
2019-08-29 19:25:03 +1000

    Allow admin users to see all contact info.

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 export const ENV = {
   mode: 'Production',
-  version: '0.3.3',
+  version: '0.3.4',
   // apiServer: 'http://35.201.19.92',
   apiServer: '',  // apiServer is now stored in firebase!
   firebaseConfig: {
